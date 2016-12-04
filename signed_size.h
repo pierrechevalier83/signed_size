a275608f092038c885cd06997092faabc0e68658
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-12-04 12:23:12 +0000

    Rename template type to Signed
    SizedIntegral was a bit long-winded.

--- signed_size.h
+++ signed_size.h
@@ -8,10 +8,10 @@
 // of the container can be represented by an int.
 // If int is too small, use sane_size<int64_t>(container).
 
-template <typename SignedIntegral = int, typename Container>
-constexpr SignedIntegral signed_size(const Container &c) {
-    static_assert(std::is_signed<SignedIntegral>(), "signed_size expects signed template parameter");
+template <typename Signed = int, typename Container>
+constexpr Signed signed_size(const Container &c) {
+    static_assert(std::is_signed<Signed>(), "signed_size expects signed template parameter");
     const auto size = c.size();
-    assert(size <= static_cast<decltype(size)>(std::numeric_limits<SignedIntegral>::max()));
-    return static_cast<SignedIntegral>(size);
+    assert(size <= static_cast<decltype(size)>(std::numeric_limits<Signed>::max()));
+    return static_cast<Signed>(size);
 }
