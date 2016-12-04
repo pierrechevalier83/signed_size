d1f0ae00989b61e2ca69b5df44387cd5051f7f26
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-12-04 18:09:17 +0000

    Backward compatibility with C++11
    Variable declaration and use of assert in a constexpr function are
    C++14 extensions.

--- signed_size.h
+++ signed_size.h
@@ -9,7 +9,11 @@
 // If int is too small, use sane_size<int64_t>(container).
 
 template <typename Signed = int, typename Container>
+#if __cplusplus==201402L
 constexpr Signed signed_size(const Container &c) {
+#else
+/*constexpr*/ Signed signed_size(const Container &c) {
+#endif
     static_assert(std::is_signed<Signed>(), "signed_size expects signed template parameter");
     const auto size = c.size();
     assert(size <= static_cast<decltype(size)>(std::numeric_limits<Signed>::max()));
